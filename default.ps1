061e438a98bec8beafcba3e1e687e6f2218cda84
Peter Seale <peter@pseale.com>
2011-08-16 00:05:16 -0500

    added versioning to build script

--- default.ps1
+++ default.ps1
@@ -1,7 +1,17 @@
 properties {
+  $baseDirectory = resolve-path .
   $buildDropFolder = 'C:\temp\build'
   $packageDropFolder = 'C:\temp\package'
   $environments = @('dev', 'qa', 'staging', 'test', 'training', 'acceptance', 'prod')
+  $versionNumber = "1.1.1.1" #Don't do this. for real version numbers, generate the version number in TeamCity and pass it into the script as an argument somehow.
+  $assemblyInfo = @{
+    "title" = "HODUGWeb";
+    "description" = 'Web application demo for HODUG';
+    "company" = "HODUG Enterprises";
+    "product" = "HODUGWeb";
+    "version" = $versionNumber;
+    "copyright" = "Copyright 2011 HODUG Enterprises. All Rights Reserved";
+  }
   
   $appSettings = @{
     "dev" = @{ "VIPName" = "Peter in our DEV ENVIRONMENT" };
@@ -16,20 +26,17 @@ properties {
 task default -depends build
 
 task compile {
+    dir -path $baseDirectory -include "AssemblyInfo.cs" -recurse | foreach {
+        write-host "Editing $($_.fullname)"
+        $assemblyInfo["file"] = $_.fullname
+        Generate-AssemblyInfo @assemblyInfo #Note "@" is the splatting operator. This means dictionary keys of the "$assemblyInfo" hashtable are matched up against function args.
+    }
+
   & 'C:\windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe' 'MvcApplication3\MvcApplication3.csproj' "/t:Package" "/p:OutDir=$($buildDropFolder)\"
 }
 
 task build -depends compile {
-    $environments | % {
-        $folder = join-path $packageDropFolder $_
-        del -force -recurse $folder -errorAction silentlycontinue
-        mkdir $folder
-        copy -force -recurse $buildDropFolder $folder
-        Change-WebConfigUsingNativeXmlSupport $folder $_
-        if ($_ -eq "prod") {
-          Create-ITILSoxAuditorCompliantDeployScript
-        }
-    }
+       Change-WebConfigUsingNativeXmlSupport $folder $_
 }
 
 task dbmigrate {
@@ -39,6 +46,64 @@ task deploy {
   #(Continuous Deployment) deploy from CI build to dev environment automatically
   powershell ./deploy.ps1 -name dev6 -port 81 -sourceFolder "'$buildDropFolder'"
 }
+
+#-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-
+#stolen from Ayende's rhino-esb psake script
+function Generate-AssemblyInfo
+{
+param(
+[string]$clsCompliant = "true",
+[string]$title,
+[string]$description,
+[string]$company,
+[string]$product,
+[string]$copyright,
+[string]$version,
+[string]$file = $(throw "file is a required parameter.")
+)
+  $commit = Get-GitCommit
+  $asmInfo = "using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliantAttribute($clsCompliant )]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute(""$title"")]
+[assembly: AssemblyDescriptionAttribute(""$description"")]
+[assembly: AssemblyCompanyAttribute(""$company"")]
+[assembly: AssemblyProductAttribute(""$product"")]
+[assembly: AssemblyCopyrightAttribute(""$copyright"")]
+[assembly: AssemblyVersionAttribute(""$version"")]
+[assembly: AssemblyInformationalVersionAttribute(""$version / $commit"")]
+[assembly: AssemblyFileVersionAttribute(""$version"")]
+[assembly: AssemblyDelaySignAttribute(false)]
+"
+
+$dir = [System.IO.Path]::GetDirectoryName($file)
+if ([System.IO.Directory]::Exists($dir) -eq $false)
+{
+Write-Host "Creating directory $dir"
+[System.IO.Directory]::CreateDirectory($dir)
+}
+Write-Host "Generating assembly info file: $file"
+Write-Output $asmInfo > $file
+}
+
+function Get-GitCommit
+{
+$gitLog = git log --oneline -1
+return $gitLog.Split(' ')[0]
+}
+
+function Get-VersionFromGitTag
+{
+  $gitTag = git describe --tags --abbrev=0
+  return $gitTag.Replace("v", "") + ".0"
+}
+
+
+
 
 function Change-WebConfigUsingNativeXmlSupport($folder, $environ) {
   $filename = join-path $folder "web.config"
