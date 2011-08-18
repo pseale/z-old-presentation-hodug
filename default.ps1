3f77e74b19a9c60dc63d84e36f57ebab1866e0d7
Peter Seale <peter@pseale.com>
2011-08-18 02:14:35 -0500

    now runs unit tests as part of build script

--- default.ps1
+++ default.ps1
@@ -1,7 +1,8 @@
 properties {
   $baseDirectory = resolve-path .
-  $buildOutputDirectory = [IO.Path]::Combine($baseDirectory, "build")
+  $buildOutputDirectory = ([IO.Path]::Combine($baseDirectory, "build"))
   $packageOutputDirectory = ([IO.Path]::Combine($baseDirectory, "artifacts"))
+  $testResultsDirectory = ([IO.Path]::Combine((resolve-path .), "TestResults"))
   $environments = @('dev', 'qa', 'staging', 'test', 'training', 'acceptance', 'prod')
   $versionNumber = "1.1.1.1" #Don't do this. for real version numbers, generate the version number in TeamCity and pass it into the script as an argument somehow.
   $assemblyInfo = @{
@@ -25,9 +26,10 @@ properties {
 }
 
 task clean {
-  Compile-Project -msbuildTargetsString "Clean"
+  & $msbuildPath 'DemoWebApp.sln' "/t:Clean" 
   del $buildOutputDirectory -recurse -force -erroraction SilentlyContinue
   del $packageOutputDirectory -recurse -force -erroraction SilentlyContinue
+  del $testResultsDirectory -recurse -force -erroraction SilentlyContinue
   #what else would you put here? 
   #-Delete the local db?
   #-Clear local cache or other oddball filesystem artifacts you may have?
@@ -38,12 +40,36 @@ task default -depends build
 
 #default build - build deployable package with MSDeploy
 task build {
-    Compile-Project -msbuildTargetsString "Package"
+    Get-AssemblyInfoFiles | foreach {
+        write-host "CHANGING ASSEMBLYINFO.CS FILE AT: '$($_.fullname)'"
+        $assemblyInfo["file"] = $_.fullname
+        Generate-AssemblyInfo @assemblyInfo #Note "@" is the splatting operator. This means dictionary keys of the "$assemblyInfo" hashtable are matched up against function args.
+    }
+    
+    Run-MSBuild -project 'DemoWebApp\DemoWebApp.csproj' -targets "Build" -configuration "Release" -outdir $buildOutputDirectory
+    Get-AssemblyInfoFiles | foreach {
+      git checkout """$($_.fullname)"""
+    }
+    Run-MSBuild -project 'DemoWebApp.Tests\DemoWebApp.Tests.csproj' -targets "Build" -configuration "Debug" -outdir $testResultsDirectory
+    Run-NUnit -filename ([IO.Path]::Combine($testResultsDirectory, "DemoWebApp.Tests.dll"))
 }
 
-task buildWithManualXmlEdits {
+task deployithManualXmlEdits {
     if ($environment -eq $null) { throw 'Environment parameter is null but must be set to run a build with manual XML edits! Call psake with it set, e.g. ''psake -parameters @{"environment"="dev"}''' }
-    Compile-Project -msbuildTargetsString "Build"
+
+    Get-AssemblyInfoFiles | foreach {
+        write-host "CHANGING ASSEMBLYINFO.CS FILE AT: '$($_.fullname)'"
+        $assemblyInfo["file"] = $_.fullname
+        Generate-AssemblyInfo @assemblyInfo #Note "@" is the splatting operator. This means dictionary keys of the "$assemblyInfo" hashtable are matched up against function args.
+    }
+    
+    Run-MSBuild -project 'DemoWebApp\DemoWebApp.csproj' -targets "Build" -configuration "Release" -outdir $buildOutputDirectory
+    Get-AssemblyInfoFiles | foreach {
+      git checkout """$($_.fullname)"""
+    }
+    Run-MSBuild -project 'DemoWebApp.Tests\DemoWebApp.Tests.csproj' -targets "Build" -configuration "Debug" -outdir $testResultsDirectory
+    Run-NUnit -filename ([IO.Path]::Combine($testResultsDirectory, "DemoWebApp.Tests.dll"))
+    
     Copy -recurse -path ([IO.Path]::Combine($buildOutputDirectory, "_PublishedWebsites\MvcApplication3")) -destination $packageOutputDirectory
     Change-WebConfigUsingNativeXmlSupport -folder $packageOutputDirectory -environment $environment
 }
@@ -56,21 +82,29 @@ task deploy {
   #powershell ./deploy.ps1 -name dev6 -port 81 -sourceFolder "'$buildDropFolder'"
 }
 
-#-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-
-#stolen from Ayende's rhino-esb psake script
 
-function Compile-Project($msbuildTargetsString) {
-    dir -path $baseDirectory -include "AssemblyInfo.cs" -recurse | foreach {
-        write-host "CHANGING ASSEMBLYINFO.CS FILE AT: '$($_.fullname)'"
-        $assemblyInfo["file"] = $_.fullname
-        Generate-AssemblyInfo @assemblyInfo #Note "@" is the splatting operator. This means dictionary keys of the "$assemblyInfo" hashtable are matched up against function args.
-    }
-    
-  exec { 
-    & 'C:\windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe' 'DemoWebApp\DemoWebApp.csproj' "/t:$($msbuildTargetsString)" "/p:OutDir=$($buildOutputDirectory)\"
-  }
+#-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-
+
+function Get-AssemblyInfoFiles {
+  dir -path $baseDirectory -include "AssemblyInfo.cs" -recurse
 }
 
+$msbuildPath = 'C:\windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe'
+function Run-MSBuild($project, $targets, $configuration, $outdir) {
+  if (-not ($outdir.EndsWith("\"))) {
+    $outdir += '\' #MSBuild requires OutDir end with a trailing slash #awesome
+  }
+
+  exec { & $msBuildPath """$project""" "/t:$($targets)" "/p:Configuration=""$configuration""" "/p:OutDir=$($outdir)" }
+}
+
+#stolen from http://codecampserver.codeplex.com/SourceControl/changeset/view/4755c1386bff#default.ps1
+function Run-NUnit($filename)
+{
+    exec { & '.\packages\NUnit.2.5.10.11092\tools\nunit-console.exe' """$filename""" '/nologo' '/nodots' '/xml=TestResults.xml' }
+}
+
+#stolen from Ayende's rhino-esb psake script
 function Generate-AssemblyInfo {
     param(
     [string]$clsCompliant = "true",
@@ -112,16 +146,19 @@ using System.Runtime.InteropServices;
     Write-Output $asmInfo > $file
 }
 
+#stolen from Ayende's rhino-esb psake script
 function Get-GitCommit {
   $gitLog = git log --oneline -1
   return $gitLog.Split(' ')[0]
 }
 
+#stolen from Ayende's rhino-esb psake script
 function Get-VersionFromGitTag {
   $gitTag = git describe --tags --abbrev=0
   return $gitTag.Replace("v", "") + ".0"
 }
 
+#stolen from a blog post
 function Change-WebConfigUsingNativeXmlSupport($folder, $environment) {
   $filename = join-path $folder "web.config"
   $webConfig = [xml](cat ($filename))
