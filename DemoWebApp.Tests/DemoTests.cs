4ae538cdcccb1e7ac07309108a22254e128b9a57
Peter Seale <peter@pseale.com>
2011-08-18 00:35:52 -0500

    Added test project; added better psake launcher shortcut

---
+++ DemoWebApp.Tests/DemoTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace DemoWebApp.Tests
+{
+    [TestFixture]
+    public class DemoTests
+    {
+        [Test]
+        public void TestWillPass()
+        {
+            Assert.AreEqual(1, 1);  //most excellent test ever
+        }
+
+        [Test]
+        [Ignore]
+        public void TestWillFAIL()
+        {
+            Assert.AreEqual(1, 2);  //TODO: implement test. Check in now and fix later! Good idea.
+        }
+    }
+}
